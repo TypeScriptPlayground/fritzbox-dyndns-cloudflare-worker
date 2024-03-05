f29674ad9e78e9536234f158e0f4147ce850f971
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-03-05 22:16:19 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { parseUrlParameters } from './url/parse_url_parameters.ts';
+
+const apiEndpoint = "https://api.cloudflare.com/client/v4/";
 
 export default {
   /**
@@ -5,6 +8,8 @@ export default {
    * @param request Incoming http request
    */
   async fetch(request : Request) : Promise<void> {
-    const {searchParams} = new URL(request.url)
+    const urlParameters = parseUrlParameters(new URL(request.url).searchParams)
+
+
   }
 }
