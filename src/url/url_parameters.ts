f4417887732cb848335a7dfb8b44fd14aef91ab0
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-04-22 21:03:15 +0200

    JSDoc

--- src/url/url_parameters.ts
+++ src/url/url_parameters.ts
@@ -1,7 +1,12 @@
 import { DNSRecord } from '../record/dns_record.ts';
 
+/**
+ * This interface represents all URL parameters from the request.
+ */
 export interface URLParameters {
+  /** Token to access the API. */
   token : string,
-  zoneId : string,
+  /** Zone ID to get records from. */
+  zoneId : string
   records : DNSRecord[]
 }
