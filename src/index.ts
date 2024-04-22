2b68be501cb4c6e1bf29f245edf57c13b0ed667c
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-04-22 10:56:15 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import buildUrlWithFilter from './url/build_url_with_filter.ts';
-import { ListDNSRecordsResponse } from './response/list_dns_records_response.ts';
-import { parseUrlParameters } from './url/parse_url_parameters.ts';
-import { endpoints } from './api/endpoints.ts';
-import getAllDnsRecords from './record/get_all_dns_records.ts';
-
-
-
 export default {
   /**
    * [Reference](https://developers.cloudflare.com/workers/runtime-apis/handlers/fetch/)
