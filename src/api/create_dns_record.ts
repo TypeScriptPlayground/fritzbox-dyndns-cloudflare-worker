7505a0c44bc87239f687dfceb687d17962517854
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-08-29 15:56:14 +0200

    Create create_dns_record.ts

---
+++ src/api/create_dns_record.ts
@@ -0,0 +1,9 @@
+import { DnsRecord } from '../record/dns_record.ts';
+import { ApiRequestOptions } from './api_request_options.ts';
+
+export default function createDnsRecord(
+  record : DnsRecord,
+  options : ApiRequestOptions
+) : void {
+  
+}
