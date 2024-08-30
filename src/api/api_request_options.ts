f2c945040133d1958e580718b745e7da9b331ce0
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-08-30 08:12:02 +0200

    Update api_request_options.ts

--- src/api/api_request_options.ts
+++ src/api/api_request_options.ts
@@ -7,7 +7,5 @@ export interface ApiRequestOptions {
   /** Token to access the API. */
   token : string,
   /** Zone ID to get records from. */
-  zoneId : string,
-  /** Record ID to update a record. */
-  recordId? : string
+  zoneId : string
 }
