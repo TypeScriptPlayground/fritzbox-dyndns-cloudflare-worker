1894415a58608c1cf53c7e56d23f9807c726f445
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-10-15 16:31:49 +0200

    Update new_request.ts

--- src/logging/new_request.ts
+++ src/logging/new_request.ts
@@ -2,11 +2,7 @@ import bodyPart from './generator/body_part.ts';
 import footer from './generator/footer.ts';
 import header from './generator/header.ts';
 
-export default function newRequest() : void {
-  console.log(
-    header('New Request') +
-    bodyPart('Type: ') +
-    bodyPart('Type: ') +
-    footer()
-  )
+export default function newRequest(requestHeader : Headers) : string {
+  
+  return header('New Request') + bodyPart('Type: ') + footer()
 }
