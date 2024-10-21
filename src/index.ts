021d8061528002f2f58730ed4662fb4e9fa248ed
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-10-21 16:12:10 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
+import createDnsRecord from './api/create_dns_record.ts';
+import listDnsRecords from './api/list_dns_records.ts';
 import {parseRequestURL} from './request/url/mod.ts'
 
 export default {
   /**
-   * [Reference](https://developers.cloudflare.com/workers/runtime-apis/handlers/fetch/)
+   * [Cloudflare Reference](https://developers.cloudflare.com/workers/runtime-apis/handlers/fetch/)
    * 
    * @param request Incoming http request
    */
   async fetch(request : Request) : Promise<void> {
     const requestUrl = new URL(request.url)
-    const requestUrlParameters = parseRequestURL(requestUrl)
+    const {token, zoneId, records} = parseRequestURL(requestUrl)
     
+    const existingRecords = await listDnsRecords({token, zoneId})
+
+    existingRecords.forEach((record) => {
+      
+    })
+    records.forEach((record) => {
+      createDnsRecord(record, {token, zoneId})
+    })
   }
 }
